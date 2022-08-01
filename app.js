0cfc90ab00f2fd3b42b781fefa5a168b636081d9
Anjali Choudhary <ANJALICHOUDHARY2610@GMAIL.COM>
2022-08-01 18:01:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,15 +17,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(__dirname + '/public'));
 
-// app.use(session({
-//
-//   secret: "Our little secret.",
-//   resave: false,
-//   saveUninitialized: false
-// }));
-//
-// app.use(passport.initialize());
-// app.use(passport.session());
+
 
 app.use(session({
   secret: process.env.SECRET,
