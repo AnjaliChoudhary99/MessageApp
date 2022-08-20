d430c7f61dcfc868b882939324a7e538082118bf
Anjali Choudhary <ANJALICHOUDHARY2610@GMAIL.COM>
2022-08-20 18:40:26 +0530

    backend of message box completed

--- app.js
+++ app.js
@@ -62,24 +62,29 @@ passport.deserializeUser(MessageAppModel.deserializeUser());
 var currentUser="";
 
 
-//send home page on get request to home route
+
 app.get("/", function(req, res){
-  console.log("GET request on home route.");
   res.render("home", {signUpMessage:"", signInMessage:""});
 });
 
 
-
-
-
+//rendering the list of existing users
 app.get("/users", function(req,res){
   if(req.isAuthenticated()){
-    MessageAppModel.find({}, {_id:0, username:1}, function (err, docs) {
+    MessageAppModel.find({}, {_id:0, username:1}, function (err, docs){
       if (err){
           console.log(err);
+          res.redirect("/");
       }
       else{
-        res.render("users", {newList:docs, length:docs.length})
+      console.log("users found are " , docs);
+      var array = [];
+      for (var i = 0; i < docs.length; i++) {
+        if(docs[i].username !== currentUser){
+        array.push(docs[i]);
+        }
+      }
+        res.render("users", {array:array});
       }
     });
   }
@@ -93,36 +98,71 @@ app.get("/users", function(req,res){
 
 app.get("/messages", function(req, res){
   if(req.isAuthenticated()){
+  console.log(currentUser,"'s database has been checked");
+  console.log("refreshing the /messages route");
 
-    //needed parameters, chat array of curent users
-    //sorted array according to who all have new field set to trues.
-    //and then we will render the array
+    MessageAppModel.findOne({username:currentUser}, function(err, found){
+      if(err){
+        console.log("error in new messages list :", err);
+      }else{
+        console.log("found chat list of currentUser for message link is: ", found.chat);
+        //found.chat is an array, it is the chat array of current user,
+        //whose each element represents chat with a particular user
+        var newChatElements=[];
+        found.chat.forEach(function(item){
+          if(item.New == true){
+            newChatElements.push(item);
+          }
+        });
 
 
 
+        console.log("type of newChatElements is: ", typeof(newChatElements));
+        if(newChatElements.length == 0){
+          console.log("no such chats");
+          var message = "No new messages!"
+        }else{
+          console.log("array of elements with boolean field = true are: ");
+          newChatElements.forEach(function(item){
+          console.log(item.person);
 
-    res.render("messages");
-  }
-  else{
+          message = "";
+          });
+        }
+        res.render("messages", {newChatElements:newChatElements, message:message});
+      }
+    });
+  }else{
     res.redirect("/");
   }
 });
+
+
+
+
+
+        // });
+        // console.log("no of chats with New  field as true are: ", n);
+        // console.log("new messages are from these users : ", newList)
+        // console.log("length of new list is :", newList.length)
+        //
+        // if(newList.length ==0){
+        //
+        // }else{
+        //
+        // }
 
 
 
 
 app.get("/dashboard", function(req, res){
   if(req.isAuthenticated()){
-    res.render("dashboard");
+    res.render("dashboard", {currentUser:currentUser});
   }
   else{
     res.redirect("/");
   }
 });
-
-
-
-
 
 
 
@@ -141,41 +181,42 @@ app.get("/dashboard", function(req, res){
 //does its work to update DB's//and sends the get request to another route
 //responsible for rendering the updated page of MmessageBox
 
-app.post("/messege/:person", function(req, res){
+app.post("/messege/:reciever", function(req, res){
 
-
-
-  console.log("req.body is : ", req.body);
+  const reciever = req.params.reciever;
+  console.log("chat object from ", currentUser, " to ", reciever ," is : ", req.body);
   const sendedText = req.body.message;
-  console.log("sendedText is : ", req.body.message);
+  console.log("sended text is : ", req.body.message);
+
   const newMessage = {
     sender: currentUser,
     text:sendedText
   }
-  console.log("newMwssage object is : ", newMessage);
-  const reciever = req.params.person;
-  console.log(req.params.person , "is the reciever for message sent by ", currentUser);
 
-
+  console.log("newMwssage object made is : ", newMessage);
 
 
 
 
 //saving to sender's DB
   MessageAppModel.findOne({username:currentUser}, function(err, found){
-  if(!found)console.log("error in finding sender's DB is: ", err);
+  if(err){
+    console.log("Error is finding the sender's dB : ", err);
+  }
   else{
-    // console.log("proceding to save into the sender's DB");
-    // console.log("found is: ",found);
-    // console.log("found.chat is : ",found.chat);
+    console.log("Saving to sender's database");
+    console.log("found document for current user is: ", found);
+
     const element = found.chat.find(function(element){
         return element.person === reciever;
       }
     );
-    // console.log("element is : ", element);
+
+    console.log("element is : ", element)
 
     if(element === undefined){
-      console.log("the element returned is : undefined");
+      console.log("appending to chat array as no existing chats with reciever ", reciever);
+
       const sendedChatObject = {
         person:reciever,
         New:false,
@@ -184,13 +225,15 @@ app.post("/messege/:person", function(req, res){
           text:sendedText}]
       }
     found.chat.push(sendedChatObject) ;
-    }
-    else{
-      console.log("returned element is not undefined");
+    }else{
+      console.log("chats with ", reciever, " already exists.");
+      // element.New = false;
       element.messages.push(newMessage);
+
     }
     found.save();
-    // console.log("found.chat array is : ", found.chat);
+
+    console.log("updated doc in sender's dB is : ",found)
     console.log("successfully saved to sender's dB");
   }
 });
@@ -202,18 +245,19 @@ app.post("/messege/:person", function(req, res){
 
 //saving to reciever's DB
   MessageAppModel.findOne({username:reciever}, function(err, found){
-  if(!found)console.log("error in finding reviever's DB is: ", err);
+  if(err){
+    console.log("Error is finding the reciever's dB : ", err);
+  }
   else{
-    console.log("proceding to save into the reciever's DB");
-    // console.log("found is: ",found);
-    // console.log("found.chat is : ",found.chat);
+
     const element = found.chat.find(function(element){
         return element.person === currentUser;
       }
     );
-    // console.log("element is : ", element);
+
     if(element === undefined){
-      console.log("the element returned is : undefined");
+    console.log("appending to chat array as no existing chats with sender ", currentUser);
+
       const recievedChatObject = {
         person:currentUser,
         New:true,
@@ -224,22 +268,19 @@ app.post("/messege/:person", function(req, res){
     found.chat.push(recievedChatObject) ;
     }
     else{
-      console.log("returned element is not undefined");
+      console.log("appending the message to message array as chats with sender already exist");
+      element.New = true;
       element.messages.push(newMessage);
     }
 
     found.save();
-    // console.log("found.chat array is : ", found.chat);
+
+    console.log("updated doc in reciever's dB is : ",found)
     console.log("successfully saved to reciever's dB");
+
     res.redirect("/chat/" + reciever);
     }
-
   });
-
-
-
-
-
 });
 
 
@@ -251,71 +292,68 @@ app.post("/messege/:person", function(req, res){
 
 app.get("/chat/:person", function(req, res){
   if(req.isAuthenticated()){
-
       const reciever = req.params.person;
-      console.log("reciever is : ", reciever);
-      console.log(currentUser , " wants to chat with ", reciever);
-
 
 
 
       //set New :false for the person in current user's dataBase, once the messageBox with that person is opened
       MessageAppModel.findOne({username:currentUser}, function(err, found){
-        if(err)console.log("setting true to false, error encountered: ", err);
-
-
-        // console.log("setting true to false, so found doc is: ", found);
-        console.log("setting true to false, so found.chat is: ", found.chat);
-
-        if(found.chat.length ==0) return 0;
+        if(err){
+          console.log("error encountered in setting true to false : ", err);
+        }
         else{
+
           const element = found.chat.find(function(element){
-              if(element.person === reciever){
-                element.New = false;
-              }
+              return element.person === reciever;
             }
           );
+          console.log("setting true to false, so found.chat is: ", found.chat);
+
+          if(element === undefined){
+            console.log("no previous chats with this person");
+          }
+          else{
+            console.log("UPDATING FINALLY");
+
+            console.log("element before found.save", element);
+            element.New = false;
+
+            found.save();
+            
+            console.log("element after found.save", element);
+            console.log("boolean for the opened chat successfully set to false");
+
+
+          }
 
         }
-        console.log("setting true to false, so revised found.chat is: ", found.chat);
       });
 
 
 
 
-
-
-
-
-
-
-
-
-
-      MessageAppModel.findOne({username:reciever}, function(err, found){
-       if(err) console.log("error in rendeing messageBox is: ", err);
+      MessageAppModel.findOne({username:currentUser}, function(err, found){
+       if(err){
+         console.log("error in rendeing messageBox is: ", err)
+       }
        else{
 
-        // console.log("found is: ", found)
-        // console.log("found.chat is : ", found.chat);
-
         const element = found.chat.find(function(element){
-            return element.person === currentUser;
+            return element.person === reciever;
           }
         );
          // console.log("element found is: " , element);
 
          if(element === undefined || element == null){
-           res.render("messageBox", {person: reciever, chatList:[]})
+           res.render("messageBox", {reciever: reciever, chatList:[]})
            }
            else{
-             res.render("messageBox", {person: reciever, chatList: element.messages});
+             res.render("messageBox", {reciever: reciever, chatList: element.messages});
            }
          }
        });
   }
   else{
-
     res.redirect("/");
   }
 });
@@ -327,7 +365,7 @@ app.get("/chat/:person", function(req, res){
 
 
 
-
+// Invoking logout() will remove the req.user property and clear the login session (if any).
 
 app.get('/logout', function(req, res, next) {
   req.logout(function(err) {
@@ -338,17 +376,31 @@ app.get('/logout', function(req, res, next) {
 
 
 
+//user sign-up
+
 app.post("/", function(req, res){
-  MessageAppModel.register({username: req.body.username}, req.body.password, function(err, user){
+  MessageAppModel.find({username:req.body.username}, function(err, found){
     if(err){
-      console.log("entered post route ")
-      console.log("ERROR while registering user: ", err);
-      res.render("home", {signUpMessage:"", signInMessage:err});
+      console.log(err);
+      res.render("home", {signInMessage:err, signUpMessage:""});
     }
-    else{
-      passport.authenticate("local")(req, res, function(){
-        res.render("home", {signUpMessage:"", signInMessage:"Registered successfully. Sign-In with your details."});
+    else if(found.length == 0){
+
+      MessageAppModel.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+
+          console.log("ERROR while registering user: ", err);
+          res.render("home", {signUpMessage:err, signInMessage:err});
+        }else{
+            passport.authenticate("local")(req, res, function(){
+            res.render("home", {signUpMessage:"", signInMessage:"Registered successfully. Sign-In with your details."});
+          });
+        }
       });
+
+    }else{
+      console.log(found);
+      res.render("home", {signInMessage:"Username already taken, try others.", signUpMessage:"Username already taken, try others."});
     }
   });
 });
@@ -358,32 +410,55 @@ app.post("/", function(req, res){
 
 
 
-app.post("/login", function(req, res){
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//user log-in
+app.post("/login", function(req, res){
   const user = new MessageAppModel({
     username:req.body.username,
     password:req.body.password
   });
 
+  //Passport exposes a login() function on req (also aliased as logIn()) that can be used to establish a login session.
+  //When the login operation completes, user will be assigned to req.user
 
-currentUser=req.body.username;
+  //When a request comes through, passport behind the scenes has already interpreted the request header
+  //by deserializing the cookie and determines if it represents a user.
+  //if the user or the request header does not represent a user, the request is unauthorised.
+  //else the req.login() generates a session for a user, which represents how long a login is good for without having to re-authenticate.
 
   req.login(user, function(err){
     if(err){
       console.log(err);
     }else{
+
+      console.log("assigned req.user to the user is : ", req.user);
+      console.log("req.body is : ", req.body);
+      currentUser = req.user.username;
       passport.authenticate("local")(req, res, function(){
-        console.log("login for the user authenticated");
-        res.redirect("/dashboard");
+      res.redirect("/dashboard");
       });
     }
   });
 });
-
-
-
-
-
 
 
 
